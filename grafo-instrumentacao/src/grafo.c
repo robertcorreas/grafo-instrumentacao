eb757d36ce37521b93054cecbbf470b5aedf6fbb
Hugo Roque <hugolnx@gmail.com>
2013-12-01 10:30:09 -0200

    det08: fix, faltou inicializar uma variavel

--- grafo-instrumentacao/src/grafo.c
+++ grafo-instrumentacao/src/grafo.c
@@ -1027,7 +1027,7 @@ int ExisteOrigem(tpGrafo *pGrafo, char *nome)
          lisCondRet = LIS_CondRetOK;
          while (lisCondRet == LIS_CondRetOK)
          {
-            LIS_tpCondRet retBackSuc;
+            LIS_tpCondRet retBackSuc = LIS_CondRetOK;
             tpVertice *pVertice;
             LIS_tppLista pBackSuc;
 
